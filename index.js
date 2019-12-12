0b9fb71dac800a11465d3c9b35fbbe5af4b76744
dscheirer <doug@dscheirer.com>
2019-12-11 16:21:46 -0800

    remove redundant requires in index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 // .env reader
 require('dotenv').config()
-const curl = new (require('curl-request'))();
-const parser = require('node-html-parser');
 const telegram = require('node-telegram-bot-api');
-const crypto = require('crypto');
 const os = require('os');
-const mjs = require('moment');
 const redis = require("redis");
-const { promisify } = require('util');
-const durationParser = require('parse-duration');
 const { watchRuntime } = require('./watchRuntime');
 
 // determine redis host from environment and connect
@@ -21,13 +15,14 @@ const api = new telegram(process.env.API_TOKEN, { polling: true });
 // now create our runtime
 const watcher = new watchRuntime(api, client, process.env.CHAT_ID);
 
+// load and run
 watcher.loadSettings().then((res) => {
   // does it look like the system just started?
   if (os.uptime() < 5 * 60) {
     watcher.sendMessage("Looks like the system just restarted, uptime is " + os.uptime());
   }
 
-  // just in case, always run an initial check.  with all of the caching we're doing, 
+  // just in case, always run an initial check.  with all of the cacheing we're doing, 
   // we should not be over-communicating
   watcher.checkWines();
 });
